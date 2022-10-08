041350fa87b173ab33e5e82aa5fe26c50e3b1f96
unknown <babelly1996@gmail.com>
2022-10-08 17:33:56 +0300

    fixing issues

--- controllers/manager/acceptOrder.ts
+++ controllers/manager/acceptOrder.ts
@@ -7,25 +7,51 @@ let acceptOrder = async (
 ) => {
   let { token, deliveryEmployee, order } = data;
   let isManager = await authManager(token);
-  if (isManager) {
-    if (deliveryEmployee) {
-      if (deliveryEmployee._id) {
-        let updatedOrder = await Order.findById(order._id);
-        let employee = await DeliveryEmployee.findById(deliveryEmployee._id);
-        if (updatedOrder) {
-          updatedOrder.status = "accepted";
-          await updatedOrder.save();
-          employee?.unCompletedOrders.push(order);
-          await employee?.save();
-          socket
-            .to(`id${deliveryEmployee._id}`)
-            .emit("givenOrder", updatedOrder);
-          socket.to(`id${order.userId}`).emit("updateOrder", updatedOrder);
-          socket.emit("acceptedOrder", "done");
-        }
-      }
-    }
+
+  if (!isManager) {
+    socket.emit("acceptedOrder", { error: "un Auth" });
+    return;
   }
+  if (!deliveryEmployee) {
+    socket.emit("acceptedOrder", {
+      error: "no delivery employee provided",
+    });
+    return;
+  }
+
+  if (!deliveryEmployee._id) {
+    socket.emit("acceptedOrder", {
+      error: "no delivery employee id provided",
+    });
+    return;
+  }
+  let updatedOrder = await Order.findById(order._id);
+  let employee = await DeliveryEmployee.findById(deliveryEmployee._id);
+  if (!updatedOrder) {
+    socket.emit("acceptedOrder", {
+      error: "order not found",
+    });
+    return;
+  }
+  if (updatedOrder.status === "accepted") {
+    socket.emit("acceptedOrder", {
+      error: "already accepted",
+    });
+    return;
+  }
+  if (!employee) {
+    socket.emit("acceptedOrder", {
+      error: "employee not found",
+    });
+    return;
+  }
+  updatedOrder.status = "accepted";
+  await updatedOrder.save();
+  employee.unCompletedOrders.push(order);
+  await employee.save();
+  socket.to(`id${deliveryEmployee._id}`).emit("givenOrder", updatedOrder);
+  socket.to(`id${order.userId}`).emit("updateOrder", updatedOrder);
+  socket.emit("acceptedOrder", { order: updatedOrder, employee: employee });
 };
 
 export default acceptOrder;
